889da0546da425030064831dcf5ace294dacaa69
Audin <audin@diamondonline.com.tw>
2021-06-16 16:52:49 +0800

    add: Enum.IsDefine

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Studying_Csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Enum_IsDefine();
+        }
+
+        static void Enum_IsDefine()
+        {
+            Console.WriteLine(Enum.IsDefined(typeof(Status), 1));
+            Console.WriteLine(Enum.IsDefined(typeof(Status), 5));
+
+            // 強塞
+            Status status = (Status)5;
+            Console.WriteLine(Enum.IsDefined(typeof(Status), status));
+        }
+
+        enum Status
+        {
+            Open = 1,
+            Close = 2
+        }
+    }
+}
