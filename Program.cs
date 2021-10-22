0c4506b040042267eecbe3af0f71f1900c2f2178
Audin <audin@diamondonline.com.tw>
2021-10-22 16:51:17 +0800

    add: deep clone add: change money add: MathPow

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Linq;
 using System.Collections.Generic;
+using Newtonsoft.Json;
+using System.Collections.Concurrent;
+using System.Threading;
 
 namespace Studying_Csharp
 {
@@ -11,7 +14,13 @@ namespace Studying_Csharp
             //Enum_IsDefine();
             //ForLoop_Special();
             //List_Remove();
-            List_DeepClone();
+            //List_DeepClone();
+            //ToStringSymbol();
+            //StringParsing();
+            //DictionaryTryRemove();
+            //ChangeMoney();
+
+            //MathPow();
         }
 
         static void List_DeepClone()
@@ -90,6 +99,127 @@ namespace Studying_Csharp
             // 強塞
             Status status = (Status)5;
             Console.WriteLine(Enum.IsDefined(typeof(Status), status));
+        }
+
+        static void ToStringSymbol() => Console.WriteLine((1231234.456).ToString("N0"));
+
+        static void StringParsing()
+        {
+            // 字串解析 - 網址
+            var url = "https://news.ycombinator.com/news?p=2";
+            var find = "p=";
+            var index = url.IndexOf(find) + find.Length;
+            var target = url.Substring(index, 1);
+            Console.WriteLine("index " + index);
+            Console.WriteLine("target " + target);
+        }
+
+        static void DictionaryTryRemove()
+        {
+            ConcurrentDictionary<string, int> data = new ConcurrentDictionary<string, int>();
+
+            data.TryAdd("1", 1);
+            Console.WriteLine(JsonConvert.SerializeObject(data, Formatting.Indented));
+
+            data.TryAdd("1", 1);
+            Console.WriteLine(JsonConvert.SerializeObject(data, Formatting.Indented));
+
+            data.TryAdd("2", 2);
+            Console.WriteLine(JsonConvert.SerializeObject(data, Formatting.Indented));
+
+            data.TryRemove("2", out var remove2);
+            Console.WriteLine(remove2);
+            Console.WriteLine(JsonConvert.SerializeObject(data, Formatting.Indented));
+
+            data.TryRemove("3", out var remove3);
+            Console.WriteLine(remove3);
+            Console.WriteLine(JsonConvert.SerializeObject(data, Formatting.Indented));
+
+            data.TryAdd("5", 5);
+            data.TryRemove("5", out var remove5);
+            Console.WriteLine(remove5);
+            Console.WriteLine(JsonConvert.SerializeObject(data, Formatting.Indented));
+
+            data.TryAdd("6", 60);
+            data.TryRemove("6", out var remove6);
+            Console.WriteLine(remove6);
+            Console.WriteLine(JsonConvert.SerializeObject(data, Formatting.Indented));
+
+            ConcurrentDictionary<long, MyClass> dataMyClass = new ConcurrentDictionary<long, MyClass>();
+            dataMyClass.TryAdd(1, new MyClass { Name = "Birdy", Age = 18});
+            dataMyClass.TryAdd(2, new MyClass { Name = "Cody", Age = 20 });
+
+            //Console.WriteLine(JsonConvert.SerializeObject(dataMyClass, Formatting.Indented));
+            //dataMyClass.TryRemove(7, out var remove7);
+            //Console.WriteLine("remove7 " + JsonConvert.SerializeObject(remove7));
+
+            //dataMyClass.TryRemove(2, out var myClass2);
+            //Console.WriteLine("myClass2 " + JsonConvert.SerializeObject(myClass2));
+            //Console.WriteLine(JsonConvert.SerializeObject(dataMyClass, Formatting.Indented));
+
+
+            Console.WriteLine("-- Clone ");
+            // 不行，修改 clone，連原始值也會被改
+            //var clone = dataMyClass.ToDictionary(entry => entry.Key, entry => entry.Value);
+            //var clone = dataMyClass.Select(d => d).ToDictionary(entry => entry.Key, entry => entry.Value);
+            //clone[1].Name += " New";
+
+            // 型別會變
+            //var clone = dataMyClass.ToArray().Clone();
+            //clone[1].Name += " New";
+
+            // 得到 null 不知怎麼使用
+            //var clone = dataMyClass.ToArray().Clone() as ConcurrentDictionary<long, MyClass>; 
+
+            // Json 可以
+            Console.WriteLine(dataMyClass.GetType());
+            var clone = JsonConvert.DeserializeObject<ConcurrentDictionary<long, MyClass>>(JsonConvert.SerializeObject(dataMyClass));
+
+            clone[1].Name += " New";
+            Console.WriteLine(JsonConvert.SerializeObject(clone, Formatting.Indented));
+            Console.WriteLine("-- Original ");
+            Console.WriteLine(JsonConvert.SerializeObject(dataMyClass, Formatting.Indented));
+        }
+
+        static void ChangeMoney()
+        {
+            List<int> ChipType = new List<int> {
+                100, 500, 2000, 5000,
+                10000, 50000, 100000, 200000
+            };
+            List<int> result = new List<int>();
+
+            const int BetBase = 100;
+
+            int BetMinRate = new Random(Guid.NewGuid().GetHashCode()).Next(1, 3000);
+            int Target = BetMinRate * BetBase;
+
+
+            Console.WriteLine($"Target {Target.ToString("N0")}");
+            int target = Target;
+
+            var chipType = ChipType.OrderByDescending(c => c);
+            foreach (var chip in chipType)
+            {
+                var count = target / chip;
+                result.Add(count);
+                target -= chip * count;
+                Console.WriteLine($"面額 {chip.ToString("N0")} x {count} 個");
+            }
+        }
+
+        static void MathPow()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(Math.Pow(2, i));
+            }
+        }
+
+        class MyClass
+        {
+            public string Name;
+            public int Age;
         }
 
         enum Status
