9a788664666479752eff5bf4aa121432ce32a5f6
Audin Wang <audinwang@jquan.com.tw>
2022-08-02 12:13:11 +0800

    studying Loop-SwitchCase-Continue

---
+++ Studying-CsharpTests/LoopAndSwitchCaseContinue.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Studying_Csharp.Tests
+{
+    [TestClass()]
+    public class LoopAndSwitchCaseContinue
+    {
+        [TestMethod()]
+        public void LoopAndSwitchCaseContinueTest()
+        {
+            // arrange
+            var data = new List<int> { 1, 2, 3};
+            // act
+            data.ForEach(x =>
+            {
+                Console.WriteLine($"now: {x}");
+                var max = 10;
+                var counter = 0;
+                while (counter++ < max)
+                {
+                    switch(x)
+                    {
+                        case 1:
+                            Console.WriteLine(x);
+                            continue; // 會跳過此回合，所以不會印出 "while last 1"
+                        case 2:
+                            Console.WriteLine(x);
+                            continue;
+                        case 3:
+                            Console.WriteLine(x);
+                            break;
+                    }
+                    Console.WriteLine("while last 1");
+                }
+                Console.WriteLine("while end");
+
+            });
+            Console.WriteLine("loop end");
+            // assert
+        }
+    }
+}
